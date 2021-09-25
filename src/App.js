ef9c111f7716f3ae0a3b78fae4b1e254cf576b65
Francisco Astorga <fastorga@nimok.com>
2021-09-25 12:25:55 -0300

    Added functionality to add items to the cart
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

--- src/App.js
+++ src/App.js
@@ -10,17 +10,39 @@ class App extends Component {
       { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
       { name: 'Arbeja', price: 2500, img: '/productos/arbejas.jpg'},
       { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg'},
-    ]
+    ],
+    carro: []
   }
-  
+
+  agregarAlCarro = (producto) => {    
+    const { carro } = this.state
+
+    if (carro.find(x => x.name === producto.name)) {
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({ ...x, cantidad: x.cantidad + 1}) : x)
+
+        return this.setState({
+          carro: newCarro
+        })
+    }
+
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1
+      })      
+    })
+  }
+
   render() {
-    return(
+    console.log(this.state.carro)
+    return(      
       <div>
         <Navbar />
         <Layout>
           <Title />
           <Productos 
-            agregarAlCarro={() => console.log('No action')}
+            agregarAlCarro={this.agregarAlCarro}
             productos = {this.state.productos}
           />
         </Layout>
