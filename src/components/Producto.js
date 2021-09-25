103437776dcd5d76f430c80bc5b847b709dface7
Francisco Astorga <fastorga@nimok.com>
2021-09-25 10:45:13 -0300

    Listed cart items
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

---
+++ src/components/Producto.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+const styles = {
+    producto: {
+        border: 'solid 1px #eee',
+        boxShadow: '0px 5px 5px rgb(0, 0, 0, 0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        borderRadius: '5px'
+    },
+    img: {
+        width: '100%'
+    }
+}
+export default class Producto extends Component {
+    render() {
+        const {producto, agregarAlCarro}=this.props
+
+        return(
+            <div style={styles.producto}>
+                <img alt={producto.name}
+                    src={producto.img}
+                    style={styles.img}
+                />
+                <h3>{producto.name}</h3>
+                <p>{producto.price}</p>
+            </div>
+        )
+    }
+}
